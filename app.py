579c88653ca184029421baeb9fe5a7db5f9f5583
Badonke <itaykarp@gmail.com>
2026-03-10 17:54:54 +0200

    full_version1.0

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from api import users_router,announcements_router,load_router,faq_router,authentication_router
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins = [
+    "https://www.itaykarpov.com",
+    "https://itaykarpov.com"
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
