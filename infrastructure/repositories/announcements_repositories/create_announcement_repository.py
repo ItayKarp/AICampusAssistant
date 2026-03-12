d2156e52b334f1179ad8490f1857a49a8a7edcac
Badonke <itaykarp@gmail.com>
2026-03-12 08:37:44 +0200

    final_version1.5

--- infrastructure/repositories/announcements_repositories/create_announcement_repository.py
+++ infrastructure/repositories/announcements_repositories/create_announcement_repository.py
@@ -7,17 +7,23 @@ from infrastructure.db.models import Announcement, AuditLog
 class CreateAnnouncementRepository(BaseAnnouncementRepository):
     def create(self, payload, user_id):
         with self.context_manager() as session:
-            announcement = Announcement(**payload)
+            user = self.get_user(user_id, session)
+            if not user:
+                raise ValueError("User not found")
+            if user.role != "admin" and user.role != "management":
+                raise ValueError("Unauthorized access")
+            announcement = Announcement(title= payload.title, content=payload.content, is_active=True, created_at=datetime.now(), target_role=payload.target_role, created_by=user.id)
             session.add(announcement)
-            session.flush(announcement)
+
             audit = AuditLog(
                 actor_user_id=user_id,
                 action_type="CREATE_ANNOUNCEMENT",
                 target_type="announcements",
                 target_id=announcement.id,
-                details=payload.get("content"),
+                details=payload.content,
                 created_at=datetime.now()
             )
             session.add(audit)
-            session.flush(audit)
+            session.flush()
+
             return announcement
